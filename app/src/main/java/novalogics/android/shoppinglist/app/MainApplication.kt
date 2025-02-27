db16ea4ddfed4a77241cbcba2ead9475fc1cecba
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-27 20:00:33 +0530

    update add level components : (stage 04)

--- app/src/main/java/novalogics/android/shoppinglist/app/MainApplication.kt
+++ app/src/main/java/novalogics/android/shoppinglist/app/MainApplication.kt
@@ -3,5 +3,24 @@ package novalogics.android.shoppinglist.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * [MainApplication] is the application class for the app and serves as the entry point
+ * for Hilt's dependency injection. It initializes Hilt and provides application-wide
+ * dependencies.
+ *
+ * ## Key Responsibilities:
+ * - Acts as the application's entry point and initializes Hilt for dependency injection.
+ * - Provides a central place for application-level configurations and dependencies.
+ *
+ * ## Usage:
+ * - The class is annotated with [@HiltAndroidApp] to enable Hilt's code generation
+ *   and dependency injection.
+ * - Hilt automatically generates necessary components and modules for dependency injection
+ *   based on this class.
+ *
+ * ## Example:
+ * - Any application-wide dependencies (e.g., database, network, or shared preferences)
+ *   can be provided using Hilt modules and accessed throughout the app.
+ */
 @HiltAndroidApp
 class MainApplication : Application()
--- app/src/main/java/novalogics/android/shoppinglist/di/AppModule.kt
+++ app/src/main/java/novalogics/android/shoppinglist/di/AppModule.kt
@@ -8,6 +8,18 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+/**
+ * [AppModule] is a Dagger Hilt module that provides application-wide dependencies.
+ * This module is installed in the [SingletonComponent], meaning the provided dependencies
+ * will have a singleton scope and live as long as the application.
+ *
+ * This module currently provides the [Context] of the application, which can be injected
+ * into other parts of the app where needed.
+ *
+ * @Provides: Indicates that the function provides a dependency.
+ * @Singleton: Ensures that the provided dependency is a singleton.
+ * @ApplicationContext: Injects the application context provided by Hil
+ */
 
 @Module
 @InstallIn(SingletonComponent::class)
